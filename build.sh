3af187b881963eb0be7f4e7769075de6e3d2c99f
dpp0900 <_dpp@kakao.com>
2024-09-23 09:31:05 +0000

    modify build script include mysql, postgres

--- build.sh
+++ build.sh
@@ -8,12 +8,17 @@ sudo apt-get -y install make cmake build-essential vim sudo git \
     libpq-dev libyaml-cpp-dev lld llvm bison python3-fire apt-transport-https \
     curl software-properties-common gnutls-dev ltrace
 
+sudo apt-get -y install make cmake build-essential vim sudo git \
+    clang ninja-build pkg-config clang-format libpq-dev llvm libyaml-cpp-dev zlib1g-dev \
+    libreadline8 libreadline-dev bison flex python3-fire
 
-if [ ! -d "$BASE_DIR/bld" ]; then
-    mkdir -p $BASE_DIR/bld
+APT_END_TIME=$(date +%s)
+
+if [ ! -d "$BASE_DIR/mysql_bld" ]; then
+    mkdir -p $BASE_DIR/mysql_bld
 fi
 
-cd $BASE_DIR/bld
+cd $BASE_DIR/mysql_bld
 cmake $BASE_DIR/mysql/ -DDOWNLOAD_BOOST=1 -DWITH_BOOST=../boost 
 make -j20
 
@@ -30,12 +35,29 @@ bin/mysqld --initialize-insecure --user=$USER
 
 MYSQL_END_TIME=$(date +%s)
 
+if [ ! -d "$BASE_DIR/postgres_bld" ]; then
+    mkdir -p $BASE_DIR/postgres_bld
+fi
+
+cd $BASE_DIR/postgres_bld
+$BASE_DIR/postgres/configure \
+    --prefix=/usr/local/pgsql
+
+sudo make -j20
+sudo make install
+sudo mkdir /usr/local/pgsql/data
+sudo chown $USER /usr/local/pgsql/data
+
+/usr/local/pgsql/bin/initdb -D /usr/local/pgsql/data
+
+POSTGRES_END_TIME=$(date +%s)
+
 cd $BASE_DIR
 cmake -S . -B build -DCMAKE_BUILD_TYPE=Release -Wno-dev -DMYSQL=ON
 cmake --build build -j
 
 DRIVER_END_TIME=$(date +%s)
 
-echo "MYSQL BUILD TIME: $((MYSQL_END_TIME - START_TIME))s"
-echo "DRIVER BUILD TIME: $((DRIVER_END_TIME - MYSQL_END_TIME))s"
-echo "TOTAL BUILD TIME: $((DRIVER_END_TIME - START_TIME))s"
+echo "APT: $((APT_END_TIME - START_TIME))s"
+echo "MYSQL: $((MYSQL_END_TIME - APT_END_TIME))s"
+echo "POSTGRES: $((POSTGRES_END_TIME - MYSQL_END_TIME))s"
